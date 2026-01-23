bd99ce4f69acd67cc93a8323048e9247181783ba
David Stewart <stewartdavidp@gmail.com>
2026-01-22 22:50:52 -0500

    Deploy Rungs v1.0.8 (13 files)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Rungs PWA Service Worker
  * Version: 1.0.2
  */
-const CACHE_VERSION = 'v1.0.7';
+const CACHE_VERSION = 'v1.0.8';
 const CACHE_NAME = `rungs-pwa-${CACHE_VERSION}`;
 
 const CACHE_FILES = ['./', './index.html', './manifest.json'];
